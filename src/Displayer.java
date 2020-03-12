bfefc54b52addb222821b3f18957c8eb54839ca1
Guillaume Valvona <valvone96@gmail.com>
2020-03-12 16:19:00 +0100

    Initialisation

---
+++ src/Displayer.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public interface Displayer
+{
+    int getWidth();
+    int getHeight();
+    Graphics2D getGraphics();
+    void repaint();
+    void setTitle(String s);
+    void addKeyListener(KeyAdapter ka);
+}
